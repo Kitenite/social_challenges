278d5b12422b8fce970ad5f6553892d76bb63418
CodyPerakslis <CodyPerakslis@noreply.github.com>
2020-01-26 04:37:10 -0600

    user

--- lib/data_show.dart
+++ lib/data_show.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'data.dart';
 
 class DataShow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final datum = new Data();
-    datum.getAll();
+    datum.getUserInfo("perak005@umn.edu");
     return Text("Hello World");
   }
 }
--- lib/user.dart
+++
@@ -1,7 +0,0 @@
-
-
-class User {
-  String username;
-  Integer score;
-
-}
